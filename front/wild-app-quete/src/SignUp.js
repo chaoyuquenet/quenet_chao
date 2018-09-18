5c92efbf1a35eddc44ec2bd29e6adac9a2339c30
chaoyuquenet <chaoyu1979@gmail.com>
2018-09-18 23:01:30 +0200

    ajout SignUp

---
+++ front/wild-app-quete/src/SignUp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>emailRenseignéDansInput</h1>
+        <input type="email" name="email" />
+      </div>
+    );
+  }
+}
+
+export default SignUp;
