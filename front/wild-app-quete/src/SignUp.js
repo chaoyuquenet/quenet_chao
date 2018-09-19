11c713d662266b48f84f597ee1460f31185e1c9d
chaoyuquenet <chaoyu1979@gmail.com>
2018-09-19 21:52:30 +0200

    ajout un formulaire avec 5 champs

--- front/wild-app-quete/src/SignUp.js
+++ front/wild-app-quete/src/SignUp.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+      firstName: '',
+      lastName: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div>
-        <h1>emailRenseignéDansInput</h1>
-        <input type="email" name="email" />
+        <h1>Email Renseigné Dans Input</h1>
+        <form onSubmit={this.handleSubmit}>
+          Email :<br />
+          <input type="email" name="email" value={this.state.email} onChange={this.handleChange}/><br />
+          Mot de passe :<br />
+          <input type="password" name="password" value={this.state.password} onChange={this.handleChange} /><br />
+          Mot de passe de vérification :<br />
+          <input type="password" name="passwordConfirm" value={this.state.passwordConfirm} onChange={this.handleChange}/><br />
+          Prénom :<br />
+          <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} /><br />
+          Nom :<br />
+          <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} /><br />
+
+          <button type="submit">submit</button>
+        </form>
       </div>
     );
   }
